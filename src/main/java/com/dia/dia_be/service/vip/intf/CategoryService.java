f8ffd4c95e0c653d169d8d7f2793327ab0fe404d
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-12-16 17:22:32 +0900

    [feat] : 카테고리 GET API 추가

---
+++ src/main/java/com/dia/dia_be/service/vip/intf/CategoryService.java
@@ -0,0 +1,10 @@
+package com.dia.dia_be.service.vip.intf;
+
+import java.util.List;
+
+import com.dia.dia_be.dto.vip.ResponseCategoryGetDTO;
+
+public interface CategoryService {
+
+	public List<ResponseCategoryGetDTO> getCategories();
+}
