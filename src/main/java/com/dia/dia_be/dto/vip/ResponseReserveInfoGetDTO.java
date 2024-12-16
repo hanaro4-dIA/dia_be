cbc2b40faaa26ebf5ae840a7acaba8968f97f2a7
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2024-12-16 15:59:08 +0900

    [feat] : 상담 예약 전 PB이름, VIP이름 GET API
    # Conflicts:
    #	src/main/java/com/dia/dia_be/dto/vip/ResponseReserveInfoGetDTO.java

--- src/main/java/com/dia/dia_be/dto/vip/ResponseReserveInfoGetDTO.java
+++ src/main/java/com/dia/dia_be/dto/vip/ResponseReserveInfoGetDTO.java
@@ -1,18 +1,4 @@
 package com.dia.dia_be.dto.vip;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class ResponseReserveInfoGetDTO {
-	@Schema(description = "PB명", example = "안유진")
-	private String pbName;
-	@Schema(description = "고객명", example = "강재준")
-	private String vipName;
 }
