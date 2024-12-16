10785ba8c32ed2f0e7508d795d3b8f5acf8a16bc
Hana_JaeJun <wowns0912@naver.com>
2024-12-16 15:34:00 +0900

    [feat] : CustomerController의 기능, swagger 및 테스트 추가

--- src/main/java/com/dia/dia_be/dto/pb/CustomerDTO.java
+++ src/main/java/com/dia/dia_be/dto/pb/CustomerDTO.java
@@ -3,6 +3,10 @@ package com.dia.dia_be.dto.pb;
 import java.time.LocalDate;
 
 import com.dia.dia_be.domain.Customer;
+import com.dia.dia_be.domain.Pb;
+import com.dia.dia_be.exception.GlobalException;
+import com.dia.dia_be.exception.PbErrorCode;
+import com.dia.dia_be.repository.PbRepository;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,23 +32,6 @@ public class CustomerDTO {
 	private int count;
 	private String memo;
 
-
-	// 엔티티를 DTO로 변환하는 메서드
-	public static CustomerDTO from(Customer customer) {
-		return CustomerDTO.builder()
-			.id(customer.getId())
-			.pbId(customer.getPb().getId())
-			.password(customer.getPassword())
-			.date(customer.getDate())
-			.count(customer.getCount())
-			.memo(customer.getMemo())
-			.email(customer.getEmail())
-			.name(customer.getName())
-			.tel(customer.getTel())
-			.address(customer.getAddress())
-			.build();
-	}
-
 	// 새로운 DTO를 생성하는 메서드
 	public static CustomerDTO of(Long id, Long pbId, String password,LocalDate date, int count, String memo, String email, String name, String tel, String address) {
 		return CustomerDTO.builder()
@@ -62,17 +49,38 @@ public class CustomerDTO {
 	}
 
 	// DTO를 엔티티로 변환하는 메서드
-	public Customer toEntity() {
+	public static Customer toEntity(CustomerDTO customerDTO, PbRepository pbRepository) {
+		Pb pb = pbRepository.findById(customerDTO.getPbId())
+			.orElseThrow(() -> new GlobalException(PbErrorCode.PROFILE_NOT_FOUND));
+
 		return Customer.builder()
-			.id(this.id)
-			.date(this.date)
-			.password(this.password)
-			.count(this.count)
-			.memo(this.memo)
-			.email(this.email)
-			.name(this.name)
-			.tel(this.tel)
-			.address(this.address)
+			.id(customerDTO.getId())
+			.pb(pb)
+			.password(customerDTO.getPassword())
+			.date(customerDTO.getDate())
+			.count(customerDTO.getCount())
+			.memo(customerDTO.getMemo())
+			.email(customerDTO.getEmail())
+			.name(customerDTO.getName())
+			.tel(customerDTO.getTel())
+			.address(customerDTO.getAddress())
 			.build();
 	}
+
+	// 엔티티를 DTO로 변환하는 메서드
+	public static CustomerDTO toDto(Customer customer) {
+		return CustomerDTO.builder()
+			.id(customer.getId())
+			.pbId(customer.getPb().getId())
+			.password(customer.getPassword())
+			.date(customer.getDate())
+			.count(customer.getCount())
+			.memo(customer.getMemo())
+			.email(customer.getEmail())
+			.name(customer.getName())
+			.tel(customer.getTel())
+			.address(customer.getAddress())
+			.build();
+	}
+
 }
