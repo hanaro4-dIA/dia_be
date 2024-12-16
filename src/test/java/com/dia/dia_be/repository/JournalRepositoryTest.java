d1b655e63ed0b1fc0ed5e3cbea4f3298ae5e6ce4
daneng4 <daneng4@naver.com>
2024-12-16 16:34:21 +0900

    [test] : journal controller & repository test

--- src/test/java/com/dia/dia_be/repository/JournalRepositoryTest.java
+++ src/test/java/com/dia/dia_be/repository/JournalRepositoryTest.java
@@ -1,10 +1,35 @@
 package com.dia.dia_be.repository;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dia.dia_be.domain.Journal;
 
 @SpringBootTest
 public class JournalRepositoryTest {
 
+	@Autowired
+	JournalRepository journalRepository;
 
+	@Test
+	@DisplayName("findAll test")
+	void findAllTest(){
+		List<Journal> journals = journalRepository.findAll();
+		assertThat(journals.size()).isGreaterThan(0);
+	}
 
+	@Test
+	@DisplayName("findById test")
+	void findByIdTest(){
+		Journal journal = journalRepository.findById(1L).orElseThrow();
+		assertThat(journal.getContents()).isNotEmpty();
+		assertThat(journal.getComplete()).isNotNull();
+	}
 }
