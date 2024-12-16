af86bf161345b634417dab531b4c87d8c385c945
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-12-16 14:21:51 +0900

    [test] : reserve 컨트롤러 테스트 추가 및 스웨거 추가

--- src/main/java/com/dia/dia_be/controller/vip/ReserveController.java
+++ src/main/java/com/dia/dia_be/controller/vip/ReserveController.java
@@ -10,6 +10,11 @@ import com.dia.dia_be.dto.vip.RequestReservePostDTO;
 import com.dia.dia_be.exception.GlobalException;
 import com.dia.dia_be.service.vip.intf.ReserveService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/vip/reserve")
 public class ReserveController {
@@ -21,6 +26,15 @@ public class ReserveController {
 	}
 
 	@PostMapping
+	@Tag(name = "상담 예약하기", description = "상담 예약 페이지 상담 예약 API")
+	@Operation(summary = "VIP가 상담 예약 양식을 채워 상담 예약을 요청", description = "예약일, 예약시, 상담 제목, 상담 내용, 카테고리를 받아 예약 등록")
+	@Parameters({
+		@Parameter(name = "date", description = "상담희망일", example = "2024-12-30"),
+		@Parameter(name = "time", description = "상담희망시", example = "14:00"),
+		@Parameter(name = "categoryId", description = "카테고리ID", example = "2"),
+		@Parameter(name = "title", description = "상담 제목", example = "퇴직연금에 가입하고 싶어요."),
+		@Parameter(name = "content", description = "상담 내용", example = "퇴직이 가까워져옵니다...")
+	})
 	public ResponseEntity<?> addReserve(@RequestBody RequestReservePostDTO requestReservePostDTO) {
 		final Long customerId = 1L;
 		try {
