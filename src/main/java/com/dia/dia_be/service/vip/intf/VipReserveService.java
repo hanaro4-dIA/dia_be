81f02d6cb78ec1505ed88528043008b01bbe7a20
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-12-17 17:06:18 +0900

    [feat] : 예약 상세 내역 조회

--- src/main/java/com/dia/dia_be/service/vip/intf/VipReserveService.java
+++ src/main/java/com/dia/dia_be/service/vip/intf/VipReserveService.java
@@ -14,4 +14,6 @@ public interface VipReserveService {
 
 	public List<ResponseReserveDTO> getReserves(Long customerId);
 
+	public ResponseReserveDTO getReserveByConsultingId(Long consultingId);
+
 }
