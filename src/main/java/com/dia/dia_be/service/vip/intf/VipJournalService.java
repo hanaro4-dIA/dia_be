8db7784388f5bbcf81ae48ef93f31e4dcc5c3563
campus2 H015 <h015@alpacoedu.onmicrosoft.com>
2024-12-17 16:23:41 +0900

    [fix] : dto 중복 파일 삭제

--- src/main/java/com/dia/dia_be/service/vip/intf/VipJournalService.java
+++ src/main/java/com/dia/dia_be/service/vip/intf/VipJournalService.java
@@ -2,7 +2,7 @@ package com.dia.dia_be.service.vip.intf;
 
 import java.util.List;
 
-import com.dia.dia_be.dto.vip.ResponseJournalDTO;
+import com.dia.dia_be.dto.vip.journalDTO.ResponseJournalDTO;
 import com.dia.dia_be.dto.vip.journalDTO.ResponseJournalScriptDTO;
 import com.dia.dia_be.dto.vip.journalDTO.ResponseSimpleJournalDTO;
 
