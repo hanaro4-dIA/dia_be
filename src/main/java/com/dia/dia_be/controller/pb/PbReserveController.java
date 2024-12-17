39491c3ec2e0cfb1c279c17abff5f4079751c03e
Joe <kavvy99@naver.com>
2024-12-17 09:48:29 +0900

    [style] : 불필요한 주석 모두 정리

--- src/main/java/com/dia/dia_be/controller/pb/PbReserveController.java
+++ src/main/java/com/dia/dia_be/controller/pb/PbReserveController.java
@@ -45,21 +45,6 @@ public class PbReserveController {
 		return pbReserveService.getApprovedReserves(status);
 	}
 
-	// @PutMapping
-	// @Tag(name = "들어온 상담 요청 관리", description = "PB의 상담 요청 관리 API")
-	// @Operation(summary = "상담 요청 승인", description = "아직 승인받지 않은 상담 요청을 승인 상태로 변경")
-	// @Parameters({
-	// 	@Parameter(name = "id", description = "승인할 상담 요청의 ID", example = "[11, 14, 16, 17]")
-	// })
-	// @ApiResponses(value = {
-	// 	@ApiResponse(responseCode = "200", description = "요청에 성공하였습니다."),
-	// 	@ApiResponse(responseCode = "400", description = "유효하지 않은 요청"),
-	// 	@ApiResponse(responseCode = "404", description = "일부 요청이 존재하지 않음"),
-	// })
-	//
-	// public void approveReserves(@RequestParam Long id) {
-	// 	pbReserveService.approveReserve(id);
-	// }
 	@PutMapping
 	@Tag(name = "들어온 상담 요청 관리", description = "PB의 상담 요청 관리 API")
 	@Operation(summary = "상담 요청 승인", description = "아직 승인받지 않은 상담 요청을 승인 상태로 변경")
@@ -70,7 +55,7 @@ public class PbReserveController {
 		@ApiResponse(responseCode = "200", description = "요청에 성공하였습니다."),
 		@ApiResponse(responseCode = "400", description = "유효하지 않은 요청"),
 		@ApiResponse(responseCode = "404", description = "상담 요청을 찾을 수 없음"),
-		@ApiResponse(responseCode = "500", description = "이미 승인된 요청입니다") // 500 에러에 대한 응답 추가
+		@ApiResponse(responseCode = "500", description = "이미 승인된 요청입니다")
 	})
 	public ResponseEntity<String> approveReserves(@RequestParam Long id) {
 		try {
