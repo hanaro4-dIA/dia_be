6d9f0812059996953cd820414e3ba38b5b6dfbcd
daneng4 <daneng4@naver.com>
2024-12-19 15:12:25 +0900

    [feat] : /vip/login/signup

--- src/main/java/com/dia/dia_be/service/vip/impl/VipLoginServiceImpl.java
+++ src/main/java/com/dia/dia_be/service/vip/impl/VipLoginServiceImpl.java
@@ -2,8 +2,37 @@ package com.dia.dia_be.service.vip.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.dia.dia_be.domain.Customer;
+import com.dia.dia_be.dto.vip.loginDTO.RequestVipSignUpDTO;
+import com.dia.dia_be.exception.GlobalException;
+import com.dia.dia_be.exception.VipErrorCode;
+import com.dia.dia_be.repository.CustomerRepository;
 import com.dia.dia_be.service.vip.intf.VipLoginService;
 
 @Service
 public class VipLoginServiceImpl implements VipLoginService {
+
+	private final CustomerRepository customerRepository;
+
+	public VipLoginServiceImpl(CustomerRepository customerRepository){
+		this.customerRepository = customerRepository;
+	}
+
+	@Override
+	public Long signupProcess(RequestVipSignUpDTO requestVipSignUpDTO) {
+		Customer customer = Customer.builder()
+			.name(requestVipSignUpDTO.getName())
+			.email(requestVipSignUpDTO.getEmail())
+			.password(requestVipSignUpDTO.getPassword())
+			.tel(requestVipSignUpDTO.getTel())
+			.address(requestVipSignUpDTO.getAddress())
+			.build();
+
+		try{
+			Customer savedCustomer = customerRepository.save(customer);
+			return savedCustomer.getId();
+		}catch (Exception e){
+			throw new GlobalException(VipErrorCode.INVALID_SIGNUP_REQUEST);
+		}
+	}
 }
--- src/main/java/com/dia/dia_be/service/vip/intf/VipLoginService.java
+++ src/main/java/com/dia/dia_be/service/vip/intf/VipLoginService.java
@@ -1,4 +1,7 @@
 package com.dia.dia_be.service.vip.intf;
 
+import com.dia.dia_be.dto.vip.loginDTO.RequestVipSignUpDTO;
+
 public interface VipLoginService {
+	public Long signupProcess(RequestVipSignUpDTO requestVipSignUpDTO);
 }
