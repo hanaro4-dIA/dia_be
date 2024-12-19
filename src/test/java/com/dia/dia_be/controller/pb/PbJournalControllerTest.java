acb7cb17158bfbe347ad294afec55542ee741677
Hana_JaeJun <wowns0912@naver.com>
2024-12-19 17:06:45 +0900

    [feat] : PbJournalController에 세션 추가

--- src/test/java/com/dia/dia_be/controller/pb/PbJournalControllerTest.java
+++ src/test/java/com/dia/dia_be/controller/pb/PbJournalControllerTest.java
@@ -7,16 +7,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.dia.dia_be.domain.Journal;
+import com.dia.dia_be.domain.PbSessionConst;
 import com.dia.dia_be.dto.pb.journalDTO.RequestJournalDTO;
+import com.dia.dia_be.dto.pb.loginDTO.LoginDTO;
 import com.dia.dia_be.repository.JournalRepository;
 import com.dia.dia_be.service.pb.intf.PbJournalService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,11 +41,24 @@ public class PbJournalControllerTest {
 	@Autowired
 	JournalRepository journalRepository;
 
+	MockHttpSession session;
+
+	@BeforeEach
+	void setUp() {
+		// 로그인 정보를 세션에 담기
+		LoginDTO loginDTO = new LoginDTO(1L);
+
+		session = new MockHttpSession();
+		session.setAttribute(PbSessionConst.LOGIN_PB, loginDTO);
+	}
+
 	@Test
 	@DisplayName("journal controller test - 상담 일지 리스트 조회")
 	void getJournals() throws Exception {
 		String url = "/pb/journals";
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.size()").isNotEmpty())
 			.andDo(print());
@@ -50,8 +67,10 @@ public class PbJournalControllerTest {
 	@Test
 	@DisplayName("journal controller test - 특정 상담 일지 조회")
 	void getJournal() throws Exception {
-		String url = "/pb/journals/" + 1;
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		String url = "/pb/journals/1";
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andDo(print());
 	}
@@ -59,10 +78,11 @@ public class PbJournalControllerTest {
 	@Test
 	@DisplayName("journal controller test - 요청 상담 내용 자세히보기 조회")
 	void getConsultingContent() throws Exception {
-		String url = "/pb/journals/reserves/" + 1 + "/content";
+		String url = "/pb/journals/reserves/1/content";
 
-		// response data가 string인지 검증
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
 			.andDo(print());
@@ -85,7 +105,10 @@ public class PbJournalControllerTest {
 
 		String requestBody = objectMapper.writeValueAsString(journalDTO);
 
-		mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestBody))
+		mockMvc.perform(post(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
 			.andExpect(status().isOk())
 			.andDo(print());
 	}
@@ -95,7 +118,9 @@ public class PbJournalControllerTest {
 	void getProductsTest() throws Exception {
 		String tag = "대출";
 		String url = "/pb/journals/products?tag=" + tag;
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.size()").isNotEmpty())
 			.andDo(print());
@@ -103,10 +128,12 @@ public class PbJournalControllerTest {
 
 	@Test
 	@DisplayName("getScripts")
-	void getScripts() throws Exception{
+	void getScripts() throws Exception {
 		Journal journal = journalRepository.findAll().get(0);
-		String url = "/pb/journals/"+journal.getId()+"/scripts";
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		String url = "/pb/journals/" + journal.getId() + "/scripts";
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.size()").isNotEmpty())
 			.andDo(print());
@@ -119,9 +146,10 @@ public class PbJournalControllerTest {
 		boolean complete = false;
 		String url = "/pb/journals/" + id + "/status?complete=" + complete;
 
-		mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+		mockMvc.perform(get(url)
+				.session(session) // 세션 추가
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.size()").isNotEmpty())
 			.andDo(print());
 	}
 }
