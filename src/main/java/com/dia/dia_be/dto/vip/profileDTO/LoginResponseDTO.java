55b38bb3b2761ca4da9608cba9b39ec3d08f4ee3
jangdayeon <dayeonjang.dev@gmail.com>
2024-12-19 14:10:15 +0900

    [feat] : login controller, service 생성

---
+++ src/main/java/com/dia/dia_be/dto/vip/profileDTO/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package com.dia.dia_be.dto.vip.profileDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginResponseDTO {
+	String message;
+}
---
+++ src/main/java/com/dia/dia_be/service/vip/impl/VipLoginServiceImpl.java
@@ -0,0 +1,9 @@
+package com.dia.dia_be.service.vip.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.dia.dia_be.service.vip.intf.VipLoginService;
+
+@Service
+public class VipLoginServiceImpl implements VipLoginService {
+}
