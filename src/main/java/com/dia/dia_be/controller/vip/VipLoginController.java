2fc39d6e0f58f5494198d8dc7e6319649c3786c9
jangdayeon <dayeonjang.dev@gmail.com>
2024-12-23 19:42:42 +0900

    [feat] : 로그인, 로그아웃, pb 정보 불러오기 session 연결 완료

--- src/main/java/com/dia/dia_be/controller/vip/VipLoginController.java
+++ src/main/java/com/dia/dia_be/controller/vip/VipLoginController.java
@@ -1,17 +1,20 @@
 package com.dia.dia_be.controller.vip;
 
+import org.apache.catalina.Session;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dia.dia_be.domain.Customer;
-import com.dia.dia_be.domain.PbSessionConst;
 import com.dia.dia_be.domain.VipSessionConst;
 import com.dia.dia_be.dto.vip.loginDTO.RequestVipSignUpDTO;
 import com.dia.dia_be.dto.vip.loginDTO.VipLoginDTO;
-import com.dia.dia_be.dto.vip.loginDTO.VipLoginResponseDTO;
+import com.dia.dia_be.global.session.SessionManager;
 import com.dia.dia_be.service.vip.intf.VipLoginService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,32 +25,46 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import com.dia.dia_be.dto.vip.loginDTO.VipLoginForm;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("vip/login")
+@RequestMapping("vip")
 public class VipLoginController {
 	private final VipLoginService vipLoginService;
-
-	public VipLoginController(VipLoginService vipLoginService) {
+	private final SessionManager sessionManager;
+	public VipLoginController(VipLoginService vipLoginService, SessionManager sessionManager) {
 		this.vipLoginService = vipLoginService;
+		this.sessionManager = sessionManager;
 	}
 
-	@PostMapping()
+	@PostMapping("/login")
 	@Operation(summary = "vip 회원 확인", description = "vip login")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "회원 로그인", content = @Content(mediaType = "application/json")),
 		@ApiResponse(responseCode = "500", description = "회원 로그인 실패")
 	})
-	public ResponseEntity<VipLoginResponseDTO> checkLogin(@RequestBody VipLoginForm vipLoginForm, HttpServletRequest request){
+	public ResponseEntity<VipLoginDTO> checkLogin(@RequestBody VipLoginForm vipLoginForm, HttpServletRequest request, HttpServletResponse response){
 		Customer customer = vipLoginService.checkLogin(vipLoginForm);
+
 		VipLoginDTO loginDTO = new VipLoginDTO(customer.getId());
 		HttpSession session = request.getSession(true);
 		session.setAttribute(VipSessionConst.LOGIN_VIP, loginDTO);
-		return ResponseEntity.ok().body(new VipLoginResponseDTO("this user exists"));
+		sessionManager.sessionCreated(session);
+		return ResponseEntity.ok().body(loginDTO);
+	}
+
+
+	@PostMapping("/logout")
+	public ResponseEntity<String> logout(HttpServletRequest request){
+		HttpSession session = sessionManager.getSession(request);
+		session.removeAttribute(VipSessionConst.LOGIN_VIP);
+		sessionManager.sessionDestroyed(session);
+		return ResponseEntity.ok().body("logout success");
 	}
 
 	@PostMapping("/signup")
--- src/main/java/com/dia/dia_be/controller/vip/VipPbController.java
+++ src/main/java/com/dia/dia_be/controller/vip/VipPbController.java
@@ -1,31 +1,63 @@
 package com.dia.dia_be.controller.vip;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dia.dia_be.domain.PbSessionConst;
+import com.dia.dia_be.domain.VipSessionConst;
+import com.dia.dia_be.dto.pb.loginDTO.LoginDTO;
+import com.dia.dia_be.dto.vip.loginDTO.VipLoginDTO;
 import com.dia.dia_be.dto.vip.pbProfileDTO.ResponsePbProfileDTO;
+import com.dia.dia_be.global.session.SessionManager;
 import com.dia.dia_be.service.vip.intf.VipPbService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/vip/pb")
+@Slf4j
 @Tag(name = "VIP의 PB 정보", description = "VIP에게 배정된 PB 정보 API")
 public class VipPbController {
 
 	private final VipPbService vipPbService;
+	private final SessionManager sessionManager;
 
-	public VipPbController(VipPbService vipPbService) {
+	public VipPbController(VipPbService vipPbService, SessionManager sessionManager) {
 		this.vipPbService = vipPbService;
+		this.sessionManager = sessionManager;
 	}
 
 	@GetMapping
 	@Operation(summary = "PB 프로필 조회", description = "특정 손님의 PB 프로필 정보를 조회합니다.")
-	public ResponseEntity<ResponsePbProfileDTO> getPbProfile() {
-		final Long customerId = 1L;
-		return ResponseEntity.ok(vipPbService.getPbProfile(customerId));
+	public ResponseEntity<?> getPbProfile(HttpServletRequest request) {
+
+
+		// // 세션 토큰을 사용해 세션을 가져옵니다.
+		HttpSession session = sessionManager.getSession(request);
+		if (session == null) {
+			return new ResponseEntity<>("No session found", HttpStatus.FOUND);
+		}
+
+		// 세션에서 VIP 로그인 정보 가져오기
+		VipLoginDTO loginDTO = (VipLoginDTO)session.getAttribute(VipSessionConst.LOGIN_VIP);
+		if (loginDTO == null) {
+			return new ResponseEntity<>("Can't find user data in session", HttpStatus.FOUND);
+		}
+
+		try {
+			// 로그인된 회원 정보로 프로필을 조회하여 반환
+			return ResponseEntity.ok(vipPbService.getPbProfile(loginDTO.getCustomerId()));
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
