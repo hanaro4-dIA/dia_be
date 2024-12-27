e687e4b2af7cb28c2ed7fa4bd63145645ce90667
jangdayeon <dayeonjang.dev@gmail.com>
2024-12-27 11:11:22 +0900

    [chore] : local 환경 설정

--- src/main/java/com/dia/dia_be/service/crawling/IssueCrawlingScheduler.java
+++ src/main/java/com/dia/dia_be/service/crawling/IssueCrawlingScheduler.java
@@ -1,5 +1,6 @@
 package com.dia.dia_be.service.crawling;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@Profile("!local")
 public class IssueCrawlingScheduler {
 
 	private final IssueCrawlingService issueCrawlingService;
